9b7903cbd6115fa8097f0344a7008c00a004e6e8
joshmun <joshuamun@gmail.com>
2017-06-15 14:27:06 -0700

    reconfig stylesheets for sass + bootstrap gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
