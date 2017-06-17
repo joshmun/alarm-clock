65712acff48ef4e8e43973d0681f71f690ef63b9
joshmun <joshuamun@gmail.com>
2017-06-17 02:24:25 -0700

    add ajax to pass user data to create controller

--- app/controllers/alarm_controller.rb
+++ app/controllers/alarm_controller.rb
@@ -11,4 +11,22 @@ class AlarmController < ApplicationController
 
   render partial: 'time', :layout => false
   end
+
+  def create
+    respond_to do |format|
+      format.js { }
+    end
+
+    user_data = params["_json"]
+    alarm_time = user_data[0]
+    phonenumber = user_data[1]
+
+    p alarm_time
+    p phonenumber
+  end
+
+  private
+    def userData
+       params.require(:alarm).permit(:"_json")
+    end
 end
